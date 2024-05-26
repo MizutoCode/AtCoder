afc8bbf0e81f7c4160f804968259942a64fc2b1a
MizutoCode <kadowaki0312.code@gmail.com>
2024-05-26 11:03:37 +0900

    starting atcoder python

---
+++ ABS/PracticeA.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May 26 10:40:31 2024
+
+@author: kadowakimizuto
+"""
+#import numpy as np
+#import matplotlib.pyplot as plt
+#import sys
+#print(sys.version_info)
+
+a = input()
+b, c = input().split()
+s = input()
+
+a = int(a)
+b = int(b)
+c = int(c)
+
+print("{} {}".format(a + b + c, s))
+
+
+
+
+
